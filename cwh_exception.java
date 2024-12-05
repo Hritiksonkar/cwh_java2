9ea94a9d1f8ce8e8a770cd2cf56c90309d876dd6
Hritiksonkar <147808870+Hritiksonkar@users.noreply.github.com>
2024-12-05 22:06:34 +0530

    Add files via upload

---
+++ cwh_exception.java
@@ -0,0 +1,18 @@
+public class cwh_exception {
+    public static void main(String[] args) {
+        int a=6000;
+        int b=0;
+        try {
+            int c=a/b;
+            System.out.println("print the divided number"+c);
+
+            
+        } catch (Exception e) {
+            System.out.println("exception term"+e);
+            
+
+        }
+        System.out.println("java excption progarm");
+    }
+    
+}
